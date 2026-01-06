ee762feb49917ed89b12794b7e4c7f5bec1cb2af
imobile <imobilelksl@gmail.com>
2026-01-06 21:26:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// Add simple scroll styling
 window.addEventListener("scroll", () => {
   const nav = document.querySelector(".nav-main");
-  if (window.scrollY > 50) nav.style.background = "#000";
-  else nav.style.background = "transparent";
-});
-// Shrink hero on scroll
-window.addEventListener('scroll', () => {
-  const hero = document.querySelector('.hero-banner');
-  if(window.scrollY > 50){ // scroll 50px පසු
-    hero.style.height = '50vh'; // shrink hero
+  const hero = document.querySelector(".hero-banner");
+
+  if (window.scrollY > 50) {
+    nav.style.background = "#000";
+    hero.style.height = "55vh";
   } else {
-    hero.style.height = '100vh'; // full screen again
+    nav.style.background = "transparent";
+    hero.style.height = "100vh";
   }
 });
